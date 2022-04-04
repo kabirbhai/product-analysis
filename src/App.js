1d299601a83ffcba5bf8b3f5e438c8026a87c3a3
kabir <makkobir79@gmail.com>
2022-04-04 14:18:54 +0600

    header component finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import NotFound from "./components/NotFound/NotFound";
+import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import NotFound from "./components/NotFound/NotFound";
 import Review from "./components/Review/Review";
 import Blog from "./components/Blog/Blog";
-import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Review" element={<Review />} />
-        <Route path="/Blog" element={<Blog />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <nav className=" flex py-6 justify-between items-center ">
+        <div className="text-3xl text-orange-500">
+          <Link to="/"> Best-Laptop</Link>
+        </div>
+        <div className="nav-item">
+          <Link className="px-1 text-xl" to="/home">
+            Home
+          </Link>
+          <Link className="px-1 text-xl" to="/review">
+            Review
+          </Link>
+          <Link className="px-1 text-xl" to="/blog">
+            blog
+          </Link>
+          <Link className="px-1 text-xl" to="/about">
+            About
+          </Link>
+          <Link className="px-1 text-xl" to="/contact">
+            Contact
+          </Link>
+        </div>
+        <div className="nav-submit">
+          <Link className="px-1 text-xl" to="/login">
+            Login
+          </Link>
+          <Link className="px-1 text-xl" to="/signup">
+            SignUp
+          </Link>
+        </div>
+      </nav>
+      <hr />
     </div>
   );
 };
